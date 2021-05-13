75d4195b42d60b784feadc13b46fac806e7efdb3
Aragami1408 <vucaominh1408@gmail.com>
2021-05-13 10:18:58 +0700

    reformat linkedlist header

--- src/linkedlist.h
+++ src/linkedlist.h
@@ -34,26 +34,37 @@ namespace handycp {
 
     template<class T>
     void push(Node<T> **head_ref, T new_data);
+
     template<class T>
     void insertAfter(Node<T> *prev_node, T new_data);
+
     template<class T>
     void append(Node<T> **head_ref, T new_data);
+
     template<class T>
     void traverse(Node<T> *node);
+
     template<class T>
     void deleteNode(Node<T> **head_ref, T key);
+
     template<class T>
     void deleteAt(Node<T> **head_ref, int pos);
+
     template<class T>
     void deleteList(Node<T> **head_ref);
+
     template<class T>
     int getCount(Node<T> *head);
+
     template<class T>
     bool search(Node<T> *head, T x);
+
     template<class T>
     T GetNth(Node<T> *head, int index);
+
     template<class T>
     T GetNthFromLast(Node<T> *head);
+
     template<class T>
     int count(Node<T> *head, int search_for); 
 };
