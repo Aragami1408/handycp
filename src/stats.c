a7948e69f907b9fe0b7a687c6c9a7e02df6ed157
Aragami1408 <vucaominh1408@gmail.com>
2021-05-21 13:16:18 +0700

    Modified Stats

--- src/stats.c
+++ src/stats.c
@@ -19,30 +19,68 @@
  *
  */
 
-#ifndef STATS_H
-#define STATS_H
-
 #include <math.h>
-
-typedef struct Stats {
-    double sum;
-    double sumsq;
-    unsigned long n;
-    double min;
-    double max;
-} Stats;
+#include "stats.h"
+#include <stdlib.h>
+#include "dbg.h"
 
 Stats *Stats_recreate(double sum, double sumsq, unsigned long n,
-        double min, double max);
+        double min, double max)
+{
+    Stats *st = (Stats *) malloc(sizeof(Stats));
+    CHECK_MEM(st, error);
 
-Stats *Stats_create();
+    st->sum = sum;
+    st->sumsq = sumsq;
+    st->n = n;
+    st->min = min;
+    st->max = max;
 
-double Stats_mean(Stats * st);
+    return st;
 
-double Stats_stddev(Stats * st);
+error:
+    return NULL;
+}
 
-void Stats_sample(Stats * st, double s);
+Stats *Stats_create()
+{
+    return Stats_recreate(0.0, 0.0, 0L, 0.0, 0.0);
+}
 
-void Stats_dump(Stats * st);
+double Stats_mean(Stats * st)
+{
+    return st->sum / st->n;
+}
 
-#endif
+double Stats_stddev(Stats * st)
+{
+    return sqrt((st->sumsq - (st->sum * st->sum / st->n)) /
+            (st->n - 1));
+}
+
+void Stats_sample(Stats * st, double s)
+{
+    st->sum += s;
+    st->sumsq += s * s;
+
+    if (st->n == 0) {
+        st->min = s;
+        st->max = s;
+    } else {
+        if (st->min > s)
+            st->min = s;
+        if (st->max < s)
+            st->max = s;
+    }
+
+    st->n += 1;
+}
+
+void Stats_dump(Stats * st)
+{
+    fprintf(stderr,
+            "sum: %f, sumsq: %f, n: %ld, "
+            "min: %f, max: %f, mean: %f, stddev: %f",
+            st->sum, st->sumsq, st->n, st->min, st->max, Stats_mean(st),
+            Stats_stddev(st));
+}
