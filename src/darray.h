57785e2eefd41908902c25ca0be37ad95330709a
Aragami1408 <vucaominh1408@gmail.com>
2021-05-26 17:24:06 +0700

    Modified DArray

--- src/darray.h
+++ src/darray.h
@@ -1,60 +1,82 @@
-/*
- * handycp - Some pretty handy stuff for Competitive Programming
- *
- * Dynamic Array
- *
- *
- * Copyright © 2021 Aragami1408/D6E093 <vucaominh1408@gmail.com>
-
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”),
- * to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
- * DEALINGS IN THE SOFTWARE.
- *
- */
 #ifndef DARRAY_H
 #define DARRAY_H
 
-#include <assert.h>
-#include <time.h>
 #include <stdlib.h>
-#include <stdbool.h>
+#include <assert.h>
 #include "dbg.h"
 
-#define INIT_CAPACITY 0
-
-typedef struct {
-    int capacity;
-    int n;
-    void **items;
+typedef struct DArray {
+    int end;
+    int max;
+    size_t element_size;
+    size_t expand_rate;
+    void **contents;
 } DArray;
 
-static bool DArray_resize(DArray *array, int new_capacity);
+DArray *DArray_create(size_t element_size, size_t initial_max);
 
-static bool DArray_enlarge(DArray *array);
+void DArray_destroy(DArray * array);
 
-DArray *DArray_create(void);
+void DArray_clear(DArray * array);
 
-int DArray_size(const DArray *array);
+int DArray_expand(DArray * array);
 
-bool DArray_add_item_at(DArray *array, int index, void *item);
+int DArray_contract(DArray * array);
 
-bool DArray_prepend_item(DArray *array, void *item);
+int DArray_push(DArray * array, void *el);
 
-bool DArray_append_item(DArray *array, void *item);
+void *DArray_pop(DArray * array);
 
-int DArray_get_item_at(const DArray *array, int index);
+void DArray_clear_destroy(DArray * array);
 
-int DArray_remove_item_at(DArray *array, int index);
+#define DArray_last(A) ((A)->contents[(A)->end - 1])
+#define DArray_first(A) ((A)->contents[0])
+#define DArray_end(A) ((A)->end)
+#define DArray_count(A) DArray_end(A)
+#define DArray_max(A) ((A)->max)
 
-int DArray_replace_item_at(DArray *array, int index, void *item);
+#define DEFAULT_EXPAND_RATE 300
 
-void DArray_free(DArray *array);
+static inline void DArray_set(DArray * array, int i, void *el)
+{
+    check(i < array->max, "darray attempt to set past max");
+    if (i > array->end)
+        array->end = i;
+    array->contents[i] = el;
+error:
+    return;
+}
+
+static inline void *DArray_get(DArray * array, int i)
+{
+    check(i < array->max, "darray attempt to get past max");
+    return array->contents[i];
+error:
+    return NULL;
+}
+
+static inline void *DArray_remove(DArray * array, int i)
+{
+    void *el = array->contents[i];
+
+    array->contents[i] = NULL;
+
+    return el;
+}
+
+static inline void *DArray_new(DArray * array)
+{
+    check(array->element_size > 0,
+            "Can't use DArray_new on 0 size darrays.");
+
+    return calloc(1, array->element_size);
+
+error:
+    return NULL;
+}
+
+#define DArray_free(E) free((E))
+
+
 
 #endif
